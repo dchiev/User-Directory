94c7067586e93de07062c2f8db0eab203e7cb484
Dan Chiev <danchiev2@Gmail.com>
2021-01-19 22:27:56 -0500

    structured components for nav and row

--- userdir/src/utils/API.js
+++ userdir/src/utils/API.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const BASEURL = "https://randomuser.me/api/";
-/* const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20"; */
+const BASEURL = "https://randomuser.me/api/?results=10&nat=us";
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
-  search: function (query) {
+  findEmployees: function () {
     return axios.get(BASEURL);
   },
 };
